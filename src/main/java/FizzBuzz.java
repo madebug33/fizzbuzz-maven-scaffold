aa6da9116037698b665a84553440cf8ae7ec69d6
戴兴华 <1650019776@qq.com>
2019-12-28 11:59:30 +0800

    pass

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -3,6 +3,10 @@ import org.junit.jupiter.api.Test;
 public class FizzBuzz {
     public static String of(int number) {
 
+        if (number % 3 == 0) {
+            return "Fizz";
+        }
 
+        return String.valueOf(number);
     }
 }
